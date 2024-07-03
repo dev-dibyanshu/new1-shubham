dc8a6565721c0e1752a2d770daa5ed7c4eac3858
Dev Dibyanshu <devdibyanshu12321@gmail.com>
2024-07-03 14:25:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Bhawna Chaudhary",
+  title: "Web Developer",
+  email: "kc1690464@gmail.com",
+  gitHub: "https://github.com/BhawnaChaudhary12",
+  instagram: "https://www.instagram.com/k_khushi1007?igsh=MXBlcG14MDQzbXViMA%3D%3D&utm_source=qr",
+  linkedIn: "https://www.linkedin.com/in/bhawna-chaudhary-4b67b0254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  medium: "English",
+  twitter: "twitter.com",
+  youTube: "https://www.youtube.com/@missstranger5283",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
